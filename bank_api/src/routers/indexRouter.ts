5c85f6a830dcfc48818ef73fc890ee1c2f9fc0a0
Kirill Parkhomenko <kxrxh.contact@gmail.com>
2023-08-27 18:19:27 +0300

    New plans for bank api

--- bank_api/src/routers/indexRouter.ts
+++ bank_api/src/routers/indexRouter.ts
@@ -1,18 +1,13 @@
 import express from 'express';
+import { plans } from './indexData';
 
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('Hello World');
+router.get('/', function(_req, res, _next) {
+  res.send({
+    "plans": plans
+  })
 });
 
-router.post('/', function(req, res, next) {
-    const successPercent = Math.round(Math.random() * 100);
-    res.status(200).send({
-      status: successPercent > 50,
-      percent: successPercent,
-    });
-  },
-);
 
 export default router;
