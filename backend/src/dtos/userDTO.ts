f39b6b99202ab9e2783fd3aecf03938fd6f75c68
BigCubeCat <bigcubecat@yandex.ru>
2023-08-27 23:05:08 +0700

    feat(dto): create dtos

---
+++ backend/src/dtos/userDTO.ts
@@ -0,0 +1,10 @@
+export type TUserDto = {
+  name: string;
+  experience: number;
+  count: number;
+  income: number;
+  consumption: number;
+  age: number;
+  phone: string;
+  provision: string;
+}
