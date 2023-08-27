e909dd512eb87bc40deb9090095f18dfe90007de
Kirill Parkhomenko <kxrxh.contact@gmail.com>
2023-08-27 18:40:46 +0300

    New structure for plans and few new plans

--- bank_api/src/routers/indexData.ts
+++ bank_api/src/routers/indexData.ts
@@ -1,142 +1,200 @@
 export const plans = [
     {
         "title": "Первый ипотечный",
-        "sumFrom": 1000000,
-        "sumTo": 5000000,
-        "term": 360,
-        "rate": 7.5
+        "type": "Ипотека",
+        "sum": { "from": 1000000, "to": 5000000 },
+        "term": { "unit": "месяц", "count": 360 },
+        "rate": 7.5,
+        "condition": {
+            "provision": "Недвижимость",
+            "user_score": 50
+        }
+    },
+    {
+        "title": "Военная ипотека",
+        "type": "Ипотека",
+        "sum": { "from": 100000, "to": 150000000 },
+        "term": { "unit": "год", "count": 20 },
+        "rate": 4.7,
+        "condition": {
+            "provision": "Нет",
+            "user_score": 100
+        }
     },
     {
         "title": "Потребительский Express",
-        "sumFrom": 50000,
-        "sumTo": 300000,
-        "term": 24,
-        "rate": 12.0
+        "type": "Потребительский",
+        "sum": { "from": 50000, "to": 300000 },
+        "term": { "unit": "месяц", "count": 24 },
+        "rate": 12.0,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 100
+        }
     },
     {
         "title": "Молодежное начало",
-        "sumFrom": 10000,
-        "sumTo": 100000,
-        "term": 12,
-        "rate": 9.8
+        "type": "Кредит",
+        "sum": { "from": 10000, "to": 100000 },
+        "term": { "unit": "месяц", "count": 12 },
+        "rate": 9.8,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 150
+        }
     },
     {
         "title": "Автофинансирование",
-        "sumFrom": 200000,
-        "sumTo": 2000000,
-        "term": 48,
-        "rate": 8.2
+        "type": "Автокредит",
+        "sum": { "from": 200000, "to": 2000000 },
+        "term": { "unit": "месяц", "count": 48 },
+        "rate": 8.2,
+        "condition": {
+            "provision": "Автомобиль",
+            "user_score": 80
+        }
     },
     {
         "title": "Рефинансирование",
-        "sumFrom": 50000,
-        "sumTo": 1500000,
-        "term": 60,
-        "rate": 6.5
+        "type": "Потребительский",
+        "sum": { "from": 50000, "to": 1500000 },
+        "term": { "unit": "месяц", "count": 60 },
+        "rate": 6.5,
+        "condition": {
+            "provision": "Имущество",
+            "user_score": 110
+        }
     },
     {
         "title": "Бизнес-старт",
-        "sumFrom": 500000,
-        "sumTo": 10000000,
-        "term": 120,
-        "rate": 14.5
+        "type": "Потребительский",
+        "sum": { "from": 500000, "to": 10000000 },
+        "term": { "unit": "год", "count": 10 },
+        "rate": 14.5,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 160
+        }
     },
     {
         "title": "Образование Plus",
-        "sumFrom": 20000,
-        "sumTo": 500000,
-        "term": 36,
-        "rate": 10.7
+        "type": "Потребительский",
+        "sum": { "from": 20000, "to": 500000 },
+        "term": { "unit": "год", "count": 36 },
+        "rate": 10.7,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 90
+        }
     },
     {
         "title": "Микрозайм Express",
-        "sumFrom": 1000,
-        "sumTo": 10000,
-        "term": 6,
-        "rate": 15.0
+        "type": "Потребительский",
+        "sum": { "from": 1000, "to": 10000 },
+        "term": { "unit": "день", "count": 90 },
+        "rate": 15.0,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 120
+        }
     },
     {
         "title": "Инвестиционное финансирование",
-        "sumFrom": 100000,
-        "sumTo": 1000000,
-        "term": 72,
-        "rate": 11.8
-    },
-    {
-        "title": "Пенсионное обеспечение",
-        "sumFrom": 50000,
-        "sumTo": 500000,
-        "term": 84,
-        "rate": 9.5
+        "type": "Потребительский",
+        "sum": { "from": 100000, "to": 1000000 },
+        "term": { "unit": "месяц", "count": 72 },
+        "rate": 11.8,
+        "condition": {
+            "provision": "Ценные бумаги",
+            "user_score": 130
+        }
     },
     {
         "title": "Строительный кредит",
-        "sumFrom": 300000,
-        "sumTo": 10000000,
-        "term": 240,
-        "rate": 9.9
+        "type": "Потребительский",
+        "sum": { "from": 300000, "to": 10000000 },
+        "term": { "unit": "месяц", "count": 240 },
+        "rate": 9.9,
+        "condition": {
+            "provision": "Недвижимость",
+            "user_score": 180
+        }
     },
     {
         "title": "Туристический заем",
-        "sumFrom": 5000,
-        "sumTo": 50000,
-        "term": 12,
-        "rate": 13.2
+        "type": "Потребительский",
+        "sum": { "from": 5000, "to": 50000 },
+        "term": { "unit": "месяц", "count": 12 },
+        "rate": 13.2,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 60
+        }
     },
     {
         "title": "Лечебный кредит",
-        "sumFrom": 20000,
-        "sumTo": 200000,
-        "term": 18,
-        "rate": 7.7
+        "type": "Потребительский",
+        "sum": { "from": 20000, "to": 200000 },
+        "term": { "unit": "месяц", "count": 18 },
+        "rate": 7.7,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 100
+        }
+    },
+    {
+        "title": "Военный лечебный кредит",
+        "type": "Потребительский",
+        "sum": { "from": 10000, "to": 2000000 },
+        "term": { "unit": "месяц", "count": 36 },
+        "rate": 3.7,
+        "condition": {
+            "provision": "Нет",
+            "user_score": 100
+        }
     },
     {
         "title": "Свадебный кредит",
-        "sumFrom": 10000,
-        "sumTo": 100000,
-        "term": 24,
-        "rate": 10.5
-    },
-    {
-        "title": "Экологическое финансирование",
-        "sumFrom": 50000,
-        "sumTo": 500000,
-        "term": 60,
-        "rate": 8.8
-    },
-    {
-        "title": "Кредит для искусств",
-        "sumFrom": 3000,
-        "sumTo": 50000,
-        "term": 36,
-        "rate": 11.2
+        "type": "Потребительский",
+        "sum": { "from": 10000, "to": 100000 },
+        "term": { "unit": "месяц", "count": 24 },
+        "rate": 10.5,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 120
+        }
     },
     {
         "title": "Сельскохозяйственный займ",
-        "sumFrom": 100000,
-        "sumTo": 3000000,
-        "term": 120,
-        "rate": 6.9
+        "type": "Потребительский",
+        "sum": { "from": 100000, "to": 3000000 },
+        "term": { "unit": "месяц", "count": 120 },
+        "rate": 6.9,
+        "condition": {
+            "provision": "Имущество",
+            "user_score": 50
+        }
     },
     {
         "title": "Стартап-финансирование",
-        "sumFrom": 500000,
-        "sumTo": 10000000,
-        "term": 180,
-        "rate": 13.5
+        "type": "Потребительский",
+        "sum": { "from": 500000, "to": 10000000 },
+        "term": { "unit": "месяц", "count": 180 },
+        "rate": 13.5,
+        "condition": {
+            "provision": "Поручительство",
+            "user_score": 180
+        }
     },
     {
         "title": "Кредитная линия для предприятий",
-        "sumFrom": 1000000,
-        "sumTo": 50000000,
-        "term": 240,
-        "rate": 9.0
+        "type": "Потребительский",
+        "sum": { "from": 1000000, "to": 50000000 },
+        "term": { "unit": "месяц", "count": 240 },
+        "rate": 9.0,
+        "condition": {
+            "provision": "Бизнес",
+            "user_score": 100
+        }
     },
-    {
-        "title": "Спортивное инвестирование",
-        "sumFrom": 50000,
-        "sumTo": 1000000,
-        "term": 72,
-        "rate": 12.8
-    }
 ]
